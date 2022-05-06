f80f9cdd8b0b8b82653a17a18ac2efdc97f07574
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-05-06 16:39:27 +0600

    get all product api ready

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleweare
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vaw2p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const fabCollection = client.db('fabFragrance').collection('inventory');
+
+        //get all product
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = fabCollection.find(query)
+            const product = await cursor.toArray();
+            res.send(product);
+        });
+    }
+    finally{}
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send('assignment 11 server is running...')

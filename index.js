70dbafc1632a932ddce191e389aa50a31fe13db6
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-05-06 16:52:40 +0600

    idividual product api ready

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -22,6 +22,14 @@ async function run() {
             const query = {};
             const cursor = fabCollection.find(query)
             const product = await cursor.toArray();
+            res.send(product);
+        });
+
+        //get individual product
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await fabCollection.findOne(query);
             res.send(product);
         });
     }

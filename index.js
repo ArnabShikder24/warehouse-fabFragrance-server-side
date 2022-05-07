5566841efb4d6785d685e4c48dd968ebc07caad3
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-05-08 05:21:41 +0600

    jot token api and functin ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function jwtVerify(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({message: 'Unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return  res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vaw2p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -16,6 +32,15 @@ async function run() {
     try {
         await client.connect();
         const fabCollection = client.db('fabFragrance').collection('inventory');
+
+        // Get Token
+        app.post('/getToken', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+               expiresIn: '1d' 
+            });
+            res.send({accessToken});
+        });
 
         //get all product
         app.get('/inventory', async (req, res) => {
@@ -79,12 +104,18 @@ async function run() {
         });
 
         // get Items with query email
-        app.get('/myItem', async (req, res) => {
+        app.get('/myItem', jwtVerify, async (req, res) => {
+            const decodedEmail = req.decoded?.email;
             const email = req.query.email;
-            const query = {email: email};
-            const cursor = fabCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
+           if(email === decodedEmail) {
+                const query = {email: email};
+                const cursor = fabCollection.find(query);
+                const items = await cursor.toArray();
+                res.send(items);
+           }
+           else {
+                res.status(403).send({message: 'Forbidden access'})
+           }
         });
     }
     finally{}
